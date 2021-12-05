b1e660e3509a351a7334d5f598b6db46aefc8584
Erico Murilo Gozzi <erico@llee.tech>
2021-12-05 19:27:42 -0300

    section 2 code.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base import Base
 
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
 def hello_world():
     return {"detail": "Hello world!"}
+
+
